a9ef9fc85632a706326d0236a55c1d3fb9813438
diallo <ibrahimasory22@hotmail.com>
2025-12-05 17:54:45 +0100

    Refactor main function for improved readability and consistency

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,29 @@
-
 use core::panic;
-use std::io::prelude::*;
-use std::io::BufReader;
 use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
 fn main() -> () {
-
-let file = File::open("input.txt").unwrap();
+    let file = File::open("input.txt").unwrap();
 
     let reader = BufReader::new(file);
     let mut res_counter = 0;
-    reader.lines().fold(50i32, |acc,line|  {
-        match line {
-            Ok(line) => {   
-                let command = line.chars().collect::<Vec<_>>();
-                let tics = command.iter().skip(1).collect::<String>();
-                let tics = tics.parse::<i32>().unwrap();
-                let res = match command[0] {
-                    'L' => {
-                        (acc - tics)%100
-                    },
-                    'R' => {
-                        (acc + tics)%100
-                    },
-                    _ => panic!("NOT SUPPORTED")
-                };
+    reader.lines().fold(50i32, |acc, line| match line {
+        Ok(line) => {
+            let command = line.chars().collect::<Vec<_>>();
+            let tics = command.iter().skip(1).collect::<String>();
+            let tics = tics.parse::<i32>().unwrap();
+            let res = match command[0] {
+                'L' => (acc - tics) % 100,
+                'R' => (acc + tics) % 100,
+                _ => panic!("NOT SUPPORTED"),
+            };
 
-                if res==0 {
-                    res_counter+=1;
-                }
-                res
-            },
-            _ => acc
+            if res == 0 {
+                res_counter += 1;
+            }
+            res
         }
+        _ => acc,
     });
-    println!("RES_COUNTER: {:?}",res_counter);
+    println!("RES_COUNTER: {:?}", res_counter);
 }
